4d840d857a4a211165906e72386f8e9e723d892c
Kimilm <gnlfla7416@gmail.com>
2022-07-22 17:13:09 +0900

    추가 - JPA 사용법 (수정)

--- src/main/java/com/sparta/week01/Week01Application.java
+++ src/main/java/com/sparta/week01/Week01Application.java
@@ -2,6 +2,7 @@ package com.sparta.week01;
 
 import com.sparta.week01.domain.Course;
 import com.sparta.week01.domain.CourseRepository;
+import com.sparta.week01.service.CourseService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,12 +20,25 @@ public class Week01Application {
 
     // Week02Application.java 의 main 함수 아래에 붙여주세요.
     @Bean
-    public CommandLineRunner demo(CourseRepository repository) {
+    public CommandLineRunner demo(CourseRepository repository, CourseService service) {
         return (args) -> {
-            Course course1 = new Course("웹개발의 봄 Spring", "남병관");
+            Course course1 = new Course("프론트엔드의 꽃 리엑트", "임민영");
             repository.save(course1);
 
             List<Course> courseList = repository.findAll();
+            courseList.forEach(c -> System.out.println(c.getTitle() + " " + c.getTutor()));
+
+            // 데이터 하나 조회하기
+            Course course = repository.findById(1L).orElseThrow(
+                    () -> new IllegalArgumentException("해당 아이디가 존재하지 않습니다.")
+            );
+
+            // 업데이트
+            Course newCourse = new Course("웹개발의 봄 Spring", "남병관");
+            service.update(1L, newCourse);
+
+            // 조회해보기
+            courseList = repository.findAll();
             courseList.forEach(c -> System.out.println(c.getTitle() + " " + c.getTutor()));
         };
     }
