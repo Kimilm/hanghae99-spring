488386cca07590daad404534d0f96e557d991d08
Kimilm <gnlfla7416@gmail.com>
2022-07-22 20:42:07 +0900

    추가 - Person Post Api

--- src/main/java/com/sparta/week01/controller/PersonController.java
+++ src/main/java/com/sparta/week01/controller/PersonController.java
@@ -1,9 +1,12 @@
 package com.sparta.week01.controller;
 
 import com.sparta.week01.domain.person.Person;
+import com.sparta.week01.domain.person.PersonRequestDto;
 import com.sparta.week01.repository.PersonRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,5 +20,11 @@ public class PersonController {
     @GetMapping("/api/persons")
     public List<Person> getPerson() {
         return personRepository.findAll();
+    }
+
+    @PostMapping("/api/persons")
+    public Person createPerson(@RequestBody PersonRequestDto requestDto) {
+        Person person = new Person(requestDto);
+        return personRepository.save(person);
     }
 }
