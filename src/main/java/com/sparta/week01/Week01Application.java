09178fc59b78e365227362e731c190578ab2e477
Kimilm <gnlfla7416@gmail.com>
2022-07-22 20:29:03 +0900

    수정 - 서버 실행시 Person 객체 추가하도록 수정

--- src/main/java/com/sparta/week01/Week01Application.java
+++ src/main/java/com/sparta/week01/Week01Application.java
@@ -1,8 +1,10 @@
 package com.sparta.week01;
 
 import com.sparta.week01.domain.course.Course;
+import com.sparta.week01.domain.person.Person;
 import com.sparta.week01.repository.CourseRepository;
 import com.sparta.week01.domain.course.CourseRequestDto;
+import com.sparta.week01.repository.PersonRepository;
 import com.sparta.week01.service.CourseService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -21,16 +23,23 @@ public class Week01Application {
 
     // Week02Application.java 의 main 함수 아래에 붙여주세요.
     @Bean
-    public CommandLineRunner demo(CourseRepository repository, CourseService service) {
+    public CommandLineRunner demo(
+            CourseRepository courseRepository,
+            CourseService service,
+            PersonRepository personRepository
+    ) {
         return (args) -> {
             Course course1 = new Course("프론트엔드의 꽃 리엑트", "임민영");
-            repository.save(course1);
+            courseRepository.save(course1);
 
-            List<Course> courseList = repository.findAll();
-            courseList.forEach(c -> System.out.println(c.getTitle() + " " + c.getTutor()));
+            Person person = new Person("손흥민", "런던", "축구선수", 28);
+            personRepository.save(person);
+
+            List<Course> courseList = courseRepository.findAll();
+            courseList.forEach(c -> System.out.println(c.getId() + " " + c.getTitle() + " " + c.getTutor()));
 
             // 데이터 하나 조회하기
-            Course course = repository.findById(1L).orElseThrow(
+            courseRepository.findById(1L).orElseThrow(
                     () -> new IllegalArgumentException("해당 아이디가 존재하지 않습니다.")
             );
 
@@ -39,7 +48,7 @@ public class Week01Application {
             service.update(1L, requestDto);
 
             // 조회해보기
-            courseList = repository.findAll();
+            courseList = courseRepository.findAll();
             courseList.forEach(c -> System.out.println(c.getTitle() + " " + c.getTutor()));
 
             // 삭제
