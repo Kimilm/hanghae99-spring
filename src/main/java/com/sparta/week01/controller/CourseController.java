7bc6101a086022f262a3d4c156092210f03bf75b
Kimilm <gnlfla7416@gmail.com>
2022-07-22 17:53:57 +0900

    추가 - Post Api

--- src/main/java/com/sparta/week01/controller/CourseController.java
+++ src/main/java/com/sparta/week01/controller/CourseController.java
@@ -2,8 +2,11 @@ package com.sparta.week01.controller;
 
 import com.sparta.week01.domain.CourseRepository;
 import com.sparta.week01.domain.Course;
+import com.sparta.week01.domain.CourseRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,5 +20,13 @@ public class CourseController {
     @GetMapping("/api/courses")
     public List<Course> getCourses() {
         return courseRepository.findAll();
+    }
+
+    // @RequestBody 어노테이션으로 넘어오는 데이터를 변환
+    @PostMapping("/api/courses")
+    public Course createCourses(@RequestBody CourseRequestDto requestDto) {
+        Course course = new Course(requestDto);
+
+        return courseRepository.save(course);
     }
 }
