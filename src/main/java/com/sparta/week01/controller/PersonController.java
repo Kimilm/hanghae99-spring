5b8439f0b20ccbceee8cbf352595254a65bf278c
Kimilm <gnlfla7416@gmail.com>
2022-07-22 20:26:41 +0900

    추가 - PersonController Get Api

--- src/main/java/com/sparta/week01/controller/PersonController.java
+++ src/main/java/com/sparta/week01/controller/PersonController.java
@@ -1,14 +1,21 @@
 package com.sparta.week01.controller;
 
 import com.sparta.week01.domain.person.Person;
+import com.sparta.week01.repository.PersonRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequiredArgsConstructor
 public class PersonController {
-    @GetMapping("/myinfo")
-    public Person getPerson() {
-        Person person = new Person("손흥민", "런던", "축구선수", 28);
-        return person;
+
+    private final PersonRepository personRepository;
+
+    @GetMapping("/api/persons")
+    public List<Person> getPerson() {
+        return personRepository.findAll();
     }
 }
