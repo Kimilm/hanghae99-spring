1f8fd58c56e1d446a1ed66f9d278941f63218efc
Kimilm <gnlfla7416@gmail.com>
2022-07-22 20:15:08 +0900

    생성 - PersonRequestDto

---
+++ src/main/java/com/sparta/week01/domain/person/PersonRequestDto.java
@@ -0,0 +1,15 @@
+package com.sparta.week01.domain.person;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class PersonRequestDto {
+    private String name;
+    private String address;
+    private String job;
+    private int age;
+}
