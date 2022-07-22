a35976679f6b1a01b68aa41bc47735d6d07e2122
Kimilm <gnlfla7416@gmail.com>
2022-07-22 16:34:15 +0900

    추가 - JPA 사용법 (입력, 조회)

--- src/main/java/com/sparta/week01/Week01Application.java
+++ src/main/java/com/sparta/week01/Week01Application.java
@@ -1,11 +1,29 @@
 package com.sparta.week01;
 
+import com.sparta.week01.domain.Course;
+import com.sparta.week01.domain.CourseRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class Week01Application {
     public static void main(String[] args) {
         SpringApplication.run(Week01Application.class, args);
+    }
+
+    // Week02Application.java 의 main 함수 아래에 붙여주세요.
+    @Bean
+    public CommandLineRunner demo(CourseRepository repository) {
+        return (args) -> {
+            Course course1 = new Course("웹개발의 봄 Spring", "남병관");
+            repository.save(course1);
+
+            List<Course> courseList = repository.findAll();
+            courseList.forEach(c -> System.out.println(c.getTitle() + " " + c.getTutor()));
+        };
     }
 }
