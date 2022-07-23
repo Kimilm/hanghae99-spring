29c9ff457872124281cdbce1d6f37a1fddd5ca0c
Kimilm <gnlfla7416@gmail.com>
2022-07-23 17:10:51 +0900

    생성 - MemoController, 추가 - Memo Get Api

---
+++ src/main/java/com/sparta/week01/controller/MemoController.java
@@ -0,0 +1,24 @@
+package com.sparta.week01.controller;
+
+import com.sparta.week01.domain.memo.Memo;
+import com.sparta.week01.domain.memo.MemoRequestDto;
+import com.sparta.week01.repository.MemoRepository;
+import com.sparta.week01.service.MemoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class MemoController {
+
+    private final MemoRepository memoRepository;
+    private final MemoService memoService;
+
+    @GetMapping("/api/memos")
+    public List<Memo> getMemos() {
+        return memoRepository.findAllByOrderByModifiedAtDesc();
+    }
+}
