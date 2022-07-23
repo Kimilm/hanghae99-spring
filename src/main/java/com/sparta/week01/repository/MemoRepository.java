40b6f37c1113118a5e5b3e56968035ba85416e07
Kimilm <gnlfla7416@gmail.com>
2022-07-23 16:58:07 +0900

    생성 - MemoRepository

---
+++ src/main/java/com/sparta/week01/repository/MemoRepository.java
@@ -0,0 +1,10 @@
+package com.sparta.week01.repository;
+
+import com.sparta.week01.domain.memo.Memo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MemoRepository extends JpaRepository<Memo, Long> {
+    List<Memo> findAllByOrderByModifiedAtDesc();
+}
