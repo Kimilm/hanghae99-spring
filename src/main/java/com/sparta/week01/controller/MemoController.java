17d8395b5661f1ed5a4093076d6f2c3260bb4a60
Kimilm <gnlfla7416@gmail.com>
2022-07-23 17:14:47 +0900

    추가 - Memo Put Api

--- src/main/java/com/sparta/week01/controller/MemoController.java
+++ src/main/java/com/sparta/week01/controller/MemoController.java
@@ -5,10 +5,7 @@ import com.sparta.week01.domain.memo.MemoRequestDto;
 import com.sparta.week01.repository.MemoRepository;
 import com.sparta.week01.service.MemoService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,5 +25,10 @@ public class MemoController {
     public Memo createMemo(@RequestBody MemoRequestDto requestDto) {
         Memo memo = new Memo(requestDto);
         return memoRepository.save(memo);
+    }
+
+    @PutMapping("/api/memos/{id}")
+    public Long updateMemo(@PathVariable long id, @RequestBody MemoRequestDto requestDto) {
+        return memoService.updateMemo(id, requestDto);
     }
 }
