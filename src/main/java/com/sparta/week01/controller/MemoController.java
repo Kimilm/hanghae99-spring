11bec813e540467d9748149bedff24fc0046e117
Kimilm <gnlfla7416@gmail.com>
2022-07-23 17:13:07 +0900

    추가 - Memo Post Api

--- src/main/java/com/sparta/week01/controller/MemoController.java
+++ src/main/java/com/sparta/week01/controller/MemoController.java
@@ -6,6 +6,8 @@ import com.sparta.week01.repository.MemoRepository;
 import com.sparta.week01.service.MemoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -20,5 +22,11 @@ public class MemoController {
     @GetMapping("/api/memos")
     public List<Memo> getMemos() {
         return memoRepository.findAllByOrderByModifiedAtDesc();
+    }
+
+    @PostMapping("/api/memos")
+    public Memo createMemo(@RequestBody MemoRequestDto requestDto) {
+        Memo memo = new Memo(requestDto);
+        return memoRepository.save(memo);
     }
 }
