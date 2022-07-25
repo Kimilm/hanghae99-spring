bcdcb2d9243af2a28c53c3c952751ae4f8e251eb
Kimilm <gnlfla7416@gmail.com>
2022-07-25 15:56:33 +0900

    추가 - Product Post Api

--- selectshop/src/main/java/com/sparta/selectshop/controller/ProductRestController.java
+++ selectshop/src/main/java/com/sparta/selectshop/controller/ProductRestController.java
@@ -1,9 +1,12 @@
 package com.sparta.selectshop.controller;
 
 import com.sparta.selectshop.models.product.Product;
+import com.sparta.selectshop.models.product.ProductRequestDto;
 import com.sparta.selectshop.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,5 +20,11 @@ public class ProductRestController {
     @GetMapping("/api/products")
     public List<Product> getProducts() {
         return productRepository.findAll();
+    }
+
+    @PostMapping("/api/products")
+    public Product createProduct(@RequestBody ProductRequestDto requestDto) {
+        Product product = new Product(requestDto);
+        return productRepository.save(product);
     }
 }
