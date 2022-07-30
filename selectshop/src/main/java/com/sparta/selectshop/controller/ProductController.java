0232ed1a17a5086f157961ddeaf7eb7bd3d124c5
Kimilm <gnlfla7416@gmail.com>
2022-07-30 16:11:48 +0900

    추가 - 메인 페이지에 유저 정보 등록

--- selectshop/src/main/java/com/sparta/selectshop/controller/ProductController.java
+++ selectshop/src/main/java/com/sparta/selectshop/controller/ProductController.java
@@ -1,13 +1,17 @@
 package com.sparta.selectshop.controller;
 
+import com.sparta.selectshop.security.model.UserDetailsImpl;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ProductController {
 
     @GetMapping("/selectshop")
-    public String selectShop() {
+    public String selectShop(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        model.addAttribute("username", userDetails.getUsername());
         return "selectshop";
     }
 }
