54889ef53430f8064f1eeea410be14fb7ecd9af6
Kimilm <gnlfla7416@gmail.com>
2022-07-30 16:20:53 +0900

    추가 - 실행시 h2DB 회원 추가

--- web/src/main/java/com/sparta/Application.java
+++ web/src/main/java/com/sparta/Application.java
@@ -6,6 +6,9 @@ import com.sparta.basic.domain.person.Person;
 import com.sparta.basic.repository.CourseRepository;
 import com.sparta.basic.repository.PersonRepository;
 import com.sparta.basic.service.CourseService;
+import com.sparta.selectshop.models.user.SignupRequestDto;
+import com.sparta.selectshop.repository.UserRepository;
+import com.sparta.selectshop.service.UserService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -24,7 +27,8 @@ public class Application {
     public CommandLineRunner demo(
             CourseRepository courseRepository,
             CourseService service,
-            PersonRepository personRepository
+            PersonRepository personRepository,
+            UserService userService
     ) {
         return (args) -> {
             Course course1 = new Course("프론트엔드의 꽃 리엑트", "임민영");
@@ -33,6 +37,22 @@ public class Application {
             Person person = new Person("손흥민", "런던", "축구선수", 28);
             personRepository.save(person);
 
+            SignupRequestDto signupRequestDto = new SignupRequestDto();
+            signupRequestDto.setUsername("kimilm");
+            signupRequestDto.setPassword("kimilm");
+            signupRequestDto.setEmail("kimilm@kimilm.com");
+
+            SignupRequestDto signupRequestDtoAdmin = new SignupRequestDto();
+            signupRequestDtoAdmin.setUsername("kimilmAdmin");
+            signupRequestDtoAdmin.setPassword("kimilmAdmin");
+            signupRequestDtoAdmin.setEmail("kimilmAdmin@kimilm.com");
+            signupRequestDtoAdmin.setAdmin(true);
+            signupRequestDtoAdmin.setAdminToken("AAABnv/xRVklrnYxKZ0aHgTBcXukeZygoC");
+
+            userService.registerUser(signupRequestDto);
+            userService.registerUser(signupRequestDtoAdmin);
+
+            /*
             List<Course> courseList = courseRepository.findAll();
             courseList.forEach(c -> System.out.println(c.getId() + " " + c.getTitle() + " " + c.getTutor()));
 
@@ -51,6 +71,7 @@ public class Application {
 
             // 삭제
             // repository.deleteById(1L);
+            */
         };
     }
 }
