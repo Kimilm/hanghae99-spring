8ae71c5d55d2f238337913b8371c646952c7ae4e
Kimilm <gnlfla7416@gmail.com>
2022-07-30 15:41:34 +0900

    추가 - 스프링 암호화 알고리즘 빈

--- selectshop/src/main/java/com/sparta/selectshop/config/security/WebSecurityConfig.java
+++ selectshop/src/main/java/com/sparta/selectshop/config/security/WebSecurityConfig.java
@@ -5,11 +5,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+
+    // 암호화 알고리즘 빈 등록
+    @Bean
+    public BCryptPasswordEncoder encodePassword() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
