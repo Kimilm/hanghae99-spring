0ac9a0f5dbbd116bf6ff9b5e8d7bc0482e892538
Kimilm <gnlfla7416@gmail.com>
2022-07-30 15:05:29 +0900

    추가 - 유저 컨트롤러 회원가입 요청 추가

--- selectshop/src/main/java/com/sparta/selectshop/controller/UserController.java
+++ selectshop/src/main/java/com/sparta/selectshop/controller/UserController.java
@@ -1,10 +1,17 @@
 package com.sparta.selectshop.controller;
 
+import com.sparta.selectshop.models.user.SignupRequestDto;
+import com.sparta.selectshop.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
+
     // 로그인 페이지
     @GetMapping("/user/login")
     public String login() {
@@ -15,5 +22,12 @@ public class UserController {
     @GetMapping("/user/signup")
     public String signup() {
         return "signup";
+    }
+
+    // 회원가입 요청 처리
+    @PostMapping("/user/signup")
+    public String registerUser(SignupRequestDto requestDto) {
+        userService.registerUser(requestDto);
+        return "redirect:/user/login";
     }
 }
