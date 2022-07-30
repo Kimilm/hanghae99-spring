4fc68a29518e9eff9d84a4694a6c02b6387a313d
Kimilm <gnlfla7416@gmail.com>
2022-07-30 13:48:53 +0900

    생성 - Spring Security 설정 생성

--- selectshop/src/main/java/com/sparta/selectshop/controller/SearchRequestController.java
+++ selectshop/src/main/java/com/sparta/selectshop/controller/SearchRequestController.java
@@ -3,7 +3,6 @@ package com.sparta.selectshop.controller;
 import com.sparta.selectshop.models.item.ItemDto;
 import com.sparta.selectshop.utils.NaverShopSearch;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
