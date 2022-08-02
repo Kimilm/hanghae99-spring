cda03345e59e68b154f6e8c51309a10fd8a31733
Kimilm <gnlfla7416@gmail.com>
2022-08-02 10:37:26 +0900

    추가 - 컨트롤러 조회 페이징 기능 추가

--- selectshop/src/main/java/com/sparta/selectshop/controller/ProductRestController.java
+++ selectshop/src/main/java/com/sparta/selectshop/controller/ProductRestController.java
@@ -4,15 +4,13 @@ import com.sparta.selectshop.models.product.Product;
 import com.sparta.selectshop.models.product.ProductMypriceRequestDto;
 import com.sparta.selectshop.models.product.ProductRequestDto;
 import com.sparta.selectshop.models.user.UserRoleEnum;
-import com.sparta.selectshop.repository.ProductRepository;
 import com.sparta.selectshop.security.model.UserDetailsImpl;
 import com.sparta.selectshop.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,18 +18,38 @@ public class ProductRestController {
 
     private final ProductService productService;
 
+    /**
+     * (관리자용) 전체 상품 조회
+     * @param page      페이지 번호
+     * @param size      한 페이지 아이템 수
+     * @param sortBy    정렬 기준
+     * @param isAsc     true: 오름차순 / false: 내림차순
+     */
     @Secured(value = UserRoleEnum.Authority.ADMIN)
     @GetMapping("/api/admin/products")
-    public List<Product> getAllProducts() {
+    public Page<Product> getAllProducts(
+            @RequestParam("page") int page,
+            @RequestParam("size") int size,
+            @RequestParam("sortBy") String sortBy,
+            @RequestParam("isAsc") boolean isAsc
+    ) {
         return productService.getAllProducts();
     }
 
+    // 로그인한 회원이 등록한 관심 상품 조회
     @GetMapping("/api/products")
-    public List<Product> getProducts(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+    public Page<Product> getProducts(
+            @RequestParam("page") int page,
+            @RequestParam("size") int size,
+            @RequestParam("sortBy") String sortBy,
+            @RequestParam("isAsc") boolean isAsc,
+            @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
         Long userId = userDetails.getUser().getId();
         return productService.getProducts(userId);
     }
 
+    // 신규 상품 등록
     @PostMapping("/api/products")
     public Product createProduct(@RequestBody ProductRequestDto requestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         Long userId = userDetails.getUser().getId();
@@ -39,6 +57,7 @@ public class ProductRestController {
         return product;
     }
 
+    // 설정 가격 변경
     @PutMapping("/api/products/{id}")
     public Long updateProduct(@PathVariable Long id, @RequestBody ProductMypriceRequestDto requestDto) {
         return productService.update(id, requestDto);
