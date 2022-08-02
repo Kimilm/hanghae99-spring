3f8bb57a69a5589443e26a3cc57588b877212482
Kimilm <gnlfla7416@gmail.com>
2022-08-03 01:23:22 +0900

    추가 - 검색 api 시간 측정

--- selectshop/src/main/java/com/sparta/selectshop/controller/SearchRequestController.java
+++ selectshop/src/main/java/com/sparta/selectshop/controller/SearchRequestController.java
@@ -1,23 +1,54 @@
 package com.sparta.selectshop.controller;
 
+import com.sparta.selectshop.models.ApiUseTime;
 import com.sparta.selectshop.models.item.ItemDto;
+import com.sparta.selectshop.models.user.User;
+import com.sparta.selectshop.repository.ApiUseTimeRepository;
+import com.sparta.selectshop.security.model.UserDetailsImpl;
 import com.sparta.selectshop.service.ItemSearchService;
-import com.sparta.selectshop.utils.NaverShopSearch;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class SearchRequestController {
 
     private final ItemSearchService itemSearchService;
 
+    private final ApiUseTimeRepository apiUseTimeRepository;
+
     @GetMapping("/api/search")
-    public List<ItemDto> execSearch(@RequestParam String query) {
-        return itemSearchService.getItems(query);
+    public List<ItemDto> execSearch(@RequestParam String query, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        long startTime = System.currentTimeMillis();
+
+        try {
+            return itemSearchService.getItems(query);
+        } finally {
+            long endTime = System.currentTimeMillis();
+
+            long runTime = endTime - startTime;
+
+            User loginUser = userDetails.getUser();
+
+            // API 사용 시간 DB 기록
+            ApiUseTime apiUseTime = apiUseTimeRepository.findByUser(loginUser).orElse(null);
+
+            if (apiUseTime == null) {
+                apiUseTime = new ApiUseTime(loginUser, runTime);
+            } else {
+                apiUseTime.addUseTime(runTime);
+            }
+
+            log.info("[API Use Time] username: " + loginUser.getUsername());
+
+            apiUseTimeRepository.save(apiUseTime);
+        }
     }
 }
