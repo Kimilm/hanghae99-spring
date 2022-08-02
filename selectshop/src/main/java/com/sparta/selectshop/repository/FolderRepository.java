1bf7fe8271de93e64a3e81894cb6945129c3a47d
Kimilm <gnlfla7416@gmail.com>
2022-08-02 16:20:25 +0900

    추가 - 폴더 생성 기능

--- selectshop/src/main/java/com/sparta/selectshop/repository/FolderRepository.java
+++ selectshop/src/main/java/com/sparta/selectshop/repository/FolderRepository.java
@@ -1,10 +1,11 @@
 package com.sparta.selectshop.repository;
 
 import com.sparta.selectshop.models.folder.Folder;
+import com.sparta.selectshop.models.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface FolderRepository extends JpaRepository<Folder, Long> {
-    List<Folder> findByUserId(Long userId);
+    List<Folder> findAllByUser(User user);
 }
--- selectshop/src/main/java/com/sparta/selectshop/service/FolderService.java
+++ selectshop/src/main/java/com/sparta/selectshop/service/FolderService.java
@@ -1,11 +1,13 @@
 package com.sparta.selectshop.service;
 
 import com.sparta.selectshop.models.folder.Folder;
+import com.sparta.selectshop.models.user.User;
 import com.sparta.selectshop.repository.FolderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -13,7 +15,17 @@ public class FolderService {
 
     private final FolderRepository folderRepository;
 
-    public List<Folder> getUserFolders(Long userId) {
-        return folderRepository.findByUserId(userId);
+    // 로그인한 회원이 등록한 모든 폴더 조회
+    public List<Folder> getUserFolders(User user) {
+        return folderRepository.findAllByUser(user);
+    }
+
+    // 유저 아이디로 폴더 생성
+    public List<Folder> addFolders(List<String> folderNames, User user) {
+        List<Folder> folderList = folderNames.stream()
+                .map(name -> new Folder(name, user))
+                .collect(Collectors.toList());
+
+        return folderRepository.saveAll(folderList);
     }
 }
