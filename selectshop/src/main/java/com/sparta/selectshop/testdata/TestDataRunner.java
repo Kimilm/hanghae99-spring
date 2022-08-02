0d6cbdd3cf4a00e6842074d3f00696e8353fff85
Kimilm <gnlfla7416@gmail.com>
2022-08-03 01:25:05 +0900

    추가 - 테스트 데이터 추가

--- selectshop/src/main/java/com/sparta/selectshop/testdata/TestDataRunner.java
+++ selectshop/src/main/java/com/sparta/selectshop/testdata/TestDataRunner.java
@@ -1,49 +1,51 @@
 package com.sparta.selectshop.testdata;
 
+import com.sparta.selectshop.controller.SearchRequestController;
 import com.sparta.selectshop.models.item.ItemDto;
 import com.sparta.selectshop.models.product.Product;
-import com.sparta.selectshop.models.user.SignupRequestDto;
 import com.sparta.selectshop.models.user.User;
 import com.sparta.selectshop.models.user.UserRoleEnum;
 import com.sparta.selectshop.repository.ProductRepository;
 import com.sparta.selectshop.repository.UserRepository;
+import com.sparta.selectshop.security.model.UserDetailsImpl;
 import com.sparta.selectshop.service.ItemSearchService;
 import com.sparta.selectshop.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import static com.sparta.selectshop.service.ProductService.MIN_MY_PRICE;
 
 @Component
+@RequiredArgsConstructor
 public class TestDataRunner implements ApplicationRunner {
 
-    @Autowired
-    UserService userService;
+    private final UserService userService;
 
-    @Autowired
-    ProductRepository productRepository;
+    private final ProductRepository productRepository;
 
-    @Autowired
-    UserRepository userRepository;
+    private final UserRepository userRepository;
 
-    @Autowired
-    PasswordEncoder passwordEncoder;
+    private final PasswordEncoder passwordEncoder;
 
-    @Autowired
-    ItemSearchService itemSearchService;
+    private final ItemSearchService itemSearchService;
+
+    private final SearchRequestController searchRequestController;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
         // 테스트 User 생성
-        User testUser = new User("슈가", passwordEncoder.encode("123"), "sugar@sparta.com", UserRoleEnum.USER);
-        testUser = userRepository.save(testUser);
+        User testUser1 = new User("정국", passwordEncoder.encode("123"), "jg@sparta.com", UserRoleEnum.USER);
+        User testUser2 = new User("제이홉", passwordEncoder.encode("123"), "hope@sparta.com", UserRoleEnum.USER);
+        User testAdminUser1 = new User("아미", passwordEncoder.encode("123"), "army@sparta.com", UserRoleEnum.ADMIN);
+        testUser1 = userRepository.save(testUser1);
+        testUser2 = userRepository.save(testUser2);
+        testAdminUser1 = userRepository.save(testAdminUser1);
 
         List<User> testUserList = new ArrayList<>();
 
@@ -55,22 +57,22 @@ public class TestDataRunner implements ApplicationRunner {
 
         // 테스트 User 의 관심상품 등록
         // 검색어 당 관심상품 10개 등록
-        createTestData(testUser, "신발");
-        createTestData(testUser, "과자");
-        createTestData(testUser, "키보드");
-        createTestData(testUser, "휴지");
-        createTestData(testUser, "휴대폰");
-        createTestData(testUser, "앨범");
-        createTestData(testUser, "헤드폰");
-        createTestData(testUser, "이어폰");
-        createTestData(testUser, "노트북");
-        createTestData(testUser, "무선 이어폰");
-        createTestData(testUser, "모니터");
+        createTestData(testUser1, "신발");
+        createTestData(testUser1, "과자");
+        createTestData(testUser1, "키보드");
+        createTestData(testUser1, "휴지");
+        createTestData(testUser1, "휴대폰");
+        createTestData(testUser1, "앨범");
+        createTestData(testUser1, "헤드폰");
+        createTestData(testUser1, "이어폰");
+        createTestData(testUser1, "노트북");
+        createTestData(testUser1, "무선 이어폰");
+        createTestData(testUser1, "모니터");
     }
 
-    private void createTestData(User user, String searchWord) throws IOException {
+    private void createTestData(User user, String searchWord) {
         // 네이버 쇼핑 API 통해 상품 검색
-        List<ItemDto> itemDtoList = itemSearchService.getItems(searchWord);
+        List<ItemDto> itemDtoList = searchRequestController.execSearch(searchWord, new UserDetailsImpl(user));
 
         List<Product> productList = new ArrayList<>();
 
