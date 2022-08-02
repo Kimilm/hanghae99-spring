ab9e7749e59440ad8686b181ff6330248a0c4eee
Kimilm <gnlfla7416@gmail.com>
2022-08-02 16:22:58 +0900

    수정 - 컨트롤러 이름 변경 추가 - 로그인한 유저의 폴더 리스트 불러오기

--- selectshop/src/main/java/com/sparta/selectshop/controller/SelectShopController.java
+++ selectshop/src/main/java/com/sparta/selectshop/controller/SelectShopController.java
@@ -1,22 +1,35 @@
 package com.sparta.selectshop.controller;
 
+import com.sparta.selectshop.models.folder.Folder;
 import com.sparta.selectshop.models.user.UserRoleEnum;
 import com.sparta.selectshop.security.model.UserDetailsImpl;
+import com.sparta.selectshop.service.FolderService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
-public class ProductController {
+@RequiredArgsConstructor
+public class SelectShopController {
+
+    private final FolderService folderService;
 
     @GetMapping("/selectshop")
     public String selectShop(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         model.addAttribute("username", userDetails.getUsername());
 
+        // 관리자 로그인시 관리자 권한 추가
         if (userDetails.getUser().getRole() == UserRoleEnum.ADMIN) {
             model.addAttribute("admin_role", true);
         }
+
+        // 유저가 생성한 모든 폴더 불러오기
+        List<Folder> folderList = folderService.getUserFolders(userDetails.getUser());
+        model.addAttribute("folders", folderList);
 
         return "selectshop";
     }
